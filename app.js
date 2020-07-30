adecda3013aa670e31e9ba852ddfd097fd2b3d3d
michel.correa <micheloscbc@hotmail.com>
2020-07-30 15:44:12 -0300

    Encapsula variaveis ambiente com dotenv

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { studentRouter } from './routes/studentRouter.js';
 
+import dotenv from 'dotenv';
+
+dotenv.config;
+
 //Conectar o Mongo pelo Mongoose
 (async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://michel:1234@cluster0.pt4wk.mongodb.net/grades?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.pt4wk.mongodb.net/grades?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -22,4 +26,4 @@ const app = express();
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3001, () => console.log('API iniciada'));
+app.listen(process.env.PORT, () => console.log('API iniciada'));
